bcedfc6085b31a9ddb8b3c04c2fe7b3756b5698d
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2019-11-19 14:34:37 -0500

    updates

--- source/aDogWaitingForYou.js
+++ source/aDogWaitingForYou.js
@@ -1,25 +1,78 @@
-$(document).ready(function() {
+// $(document).ready(function() {
  
-  $("#owl-demo").owlCarousel({
-    jsonPath : 'json/customData.json',
-    jsonSuccess : customDataSuccess
-  });
+//   $("#owl-demo").owlCarousel({
+//     jsonPath : 'json/customData.json',
+//     jsonSuccess : customDataSuccess
+//   });
  
-  function customDataSuccess(data){
-    var content = "";
-    for(var i in data["items"]){
+//   function customDataSuccess(data){
+//     var content = "";
+//     for(var i in data["items"]){
+       
+//        var img = data["items"][i].img;
+//        var alt = data["items"][i].alt;
+ 
+//        content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+//     }
+//     $("#owl-demo").html(content);
+//   }
+ 
+// });
+
+//  $(document).ready(function () { 
+//                       $.ajax
+//                         ({
+//                     method: "GET",
+//                     url: " https://lionheartrescue.github.io/newbestfriend/bestFriendFinder.json"
+//                     jsonSuccess:customDataSuccess
+//                          });
+ 
+//   function customDataSuccess(data){
+//     var content = "";
+//     for(var i in data["items"]){
+       
+//        var img = data["items"][i].img;
+//        var alt = data["items"][i].alt;
+ 
+//        content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+//     }
+//     $("#owl-demo").html(content);
+//   }
+ 
+// });
+
+
+// $(document).ready(function () {  
+//     $('#owl-demo').html('1')
+
+  
+  $(document).ready(function ()  
+            {
+                    $.ajax
+                        ({
+                    method: "GET",
+                    url: " https://lionheartrescue.github.io/newbestfriend/bestFriendFinder.json"
+                         }).done(function(data) 
+
+                               { var content = "";
+                       for(var i in data["items"]){
        
        var img = data["items"][i].img;
        var alt = data["items"][i].alt;
  
        content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
-    }
-    $("#owl-demo").html(content);
-  }
+    }          $("#owl-demo").html(content);
+                              });
+            });
+           
+
+  //          var content = "";
+  //   for(var i in data["items"]){
+       
+  //      var img = data["items"][i].img;
+  //      var alt = data["items"][i].alt;
  
-});
-
-// $(document).ready(function () {  
-//     $('#owl-demo').html('1')
-
-//     });
+  //      content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+  //   }
+  //   $("#owl-demo").html(content);
+  // }
