708654c8f3c0cb8d62b493b163a1513709efe755
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2019-11-19 15:11:04 -0500

    updates

--- source/aDogWaitingForYou.js
+++ source/aDogWaitingForYou.js
@@ -64,15 +64,4 @@
     }          $("#owl-demo").html(content);
                               });
             });
-           
-
-  //          var content = "";
-  //   for(var i in data["items"]){
-       
-  //      var img = data["items"][i].img;
-  //      var alt = data["items"][i].alt;
- 
-  //      content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
-  //   }
-  //   $("#owl-demo").html(content);
-  // }
+   
