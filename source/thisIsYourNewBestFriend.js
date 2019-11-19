f275838cbe34aba28b864cbd8dbd59f3f97621d7
Aaron Lilly <aaronlilly84@gmail.com>
2019-11-18 22:52:51 -0500

    imgs

---
+++ source/thisIsYourNewBestFriend.js
@@ -0,0 +1,30 @@
+     var baseUrl = "https://lionheartrescue.github.io/newbestfriend/img/";
+        var dogPictures = ["bella.jpg","canela.jpg","charlie.jpg","chula.jpg",]
+var iO = "<img src='" ;
+var iC = "'></img>";
+  //iO + baseUrl + dogPictures[0] +iC
+
+  var bLink = [{"link":"<a href ='https://lionheartrescue.github.io/newbestfriend/img/newbestfriend.html'>Home</a>"},{"link":"<a href='https://www.facebook.com'>Facebook</a>"}]
+
+
+//content loaders
+
+//top carosel
+// $(document).ready(function () {  
+//     $('#append').append('')
+
+//     });
+
+
+//bottom links
+console.log(bLink.length)
+
+$(document).ready(function () {  
+for (var i = 0 ; i < bLink.length; i++) {
+  $('#bLink').append("<div id='linkList'>" + bLink[i].link + " </div>")
+}
+   $('#bLink').append("<br>")
+
+    });
+
+
